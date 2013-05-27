afc6b245b87c200b5e6682a69506bd75fa860ce9
Cassiano Rocha <cassiano@minhavida.com.br>
2013-05-27 18:55:02 -0300

    Initial Commit

---
+++ gameexample/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  gameexampleAppController.h
+//  gameexample
+//
+//  Created by Cassiano Rocha on 27/05/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ gameexample/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  gameexampleAppController.h
+//  gameexample
+//
+//  Created by Cassiano Rocha on 27/05/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
